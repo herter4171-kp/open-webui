6e01c6a4ec4eec8fe45969248a82d0da550ca2e6
herter4171-kp <herter@kairospower.com>
2025-12-08 16:22:22 -0500

    Move login disclaimer more.

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -198,19 +198,16 @@
 	}}
 />
 
-<LoginDisclaimer />
-
 <div class="w-full h-screen max-h-[100dvh] text-white relative" id="auth-page">
 	<div class="w-full h-full absolute top-0 left-0 bg-white dark:bg-black"></div>
 
 	<div class="w-full absolute top-0 left-0 right-0 h-8 drag-region" />
 
 	{#if loaded}
-		<div
-			class="fixed bg-transparent min-h-screen w-full flex justify-center font-primary z-50 text-black dark:text-white"
-			id="auth-container"
-		>			
+		<div class="fixed bg-transparent min-h-screen w-full flex justify-center items-center font-primary z-50 text-black dark:text-white" id="auth-container">			
 			<div class="w-full px-10 min-h-screen flex flex-col text-center">
+				<LoginDisclaimer />
+				
 				{#if ($config?.features.auth_trusted_header ?? false) || $config?.features.auth === false}
 					<div class=" my-auto pb-10 w-full sm:max-w-md">
 						<div
