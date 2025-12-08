1d00b19f5824e0ed6298c997bfdd1fcd1376d63f
herter4171-kp <herter@kairospower.com>
2025-12-08 16:35:14 -0500

    Add scrollbar to login in case vertical height is too small.

--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -204,10 +204,10 @@
 	<div class="w-full absolute top-0 left-0 right-0 h-8 drag-region" />
 
 	{#if loaded}
-		<div class="fixed bg-transparent min-h-screen w-full flex justify-center items-center font-primary z-50 text-black dark:text-white" id="auth-container">			
+		<div class="fixed bg-transparent inset-0 overflow-y-auto w-full flex justify-center items-center font-primary z-50 text-black dark:text-white" id="auth-container">			
 			<div class="w-full px-10 min-h-screen flex flex-col text-center">
 				<LoginDisclaimer />
-				
+
 				{#if ($config?.features.auth_trusted_header ?? false) || $config?.features.auth === false}
 					<div class=" my-auto pb-10 w-full sm:max-w-md">
 						<div
